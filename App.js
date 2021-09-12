f7608f7caee5fa2467c67dbb5e6dac1ef25f3ed2
Svarto <svarto@protonmail.com>
2021-09-12 15:15:22 +0200

    removed expo-app-loader

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import AppLoading from "expo-app-loading";
 
 import {StyleSheet, Image, View, useWindowDimensions} from 'react-native';
 
@@ -9,12 +8,7 @@ export default function App() {
     const [loading, setLoading] = useState(true)
   return (
     <View style={styles.container}>
-        {loading &&    <AppLoading         // Instruct SplashScreen not to hide yet, we want to do this manually
-            autoHideSplash={true}
-            startAsync={() => {}}
-            onError={console.error}
-            onFinish={() => setTimeout(() => setLoading(false), 2000)}
-        />}
+
       <Image source={require("./assets/splash.png")} style={{width: width, height: height}} resizeMode={"contain"}/>
       <StatusBar style="auto" />
     </View>
